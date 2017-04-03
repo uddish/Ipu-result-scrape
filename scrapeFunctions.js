4bae4c2fdc9e40227400d88f1f958fe673e1c8d6
Uddish <uddish@xprep.co>
2017-04-03 21:46:23 +0530

    CS and IT faculty added

--- scrapeFunctions.js
+++ scrapeFunctions.js
@@ -11,7 +11,8 @@ function scrape()    {
 
 //fetches all the faculty names
     this.getITFaculty = function(res) {
-
+        var facultyArr = [];
+        var arr = [];
         request({
             url: 'http://it.mait.ac.in/index.php/people/faculty',
             method: "GET", 
@@ -19,28 +20,53 @@ function scrape()    {
             if(err) {
                 throw err;
             }
-            //Scrapes all the faculty names
+             //Scrapes all the faculty names
             $ = cheerio.load(body);
-            var arr = [];
-            var facultyName;
             for(var i = 0; i < $('.article-content td a').length; i++)  {
-                // facultyObject = new Object;
+                var facultyName;
                 facultyName = $('.article-content td a').eq(i).text();
                 arr.push(facultyName);
-                // facultyObject.faculty_IT = facultyName;
-                // console.log(arr);
             }
             facultyObject = {};
             for(j = 0; j < arr.length; j++) {
                 facultyObject[(j+1)] = arr[j];
             }
             ITfaculty.IT = facultyObject;
-            console.log(ITfaculty);
-        });
-         setTimeout((function() {res.send(ITfaculty)}), 1500);
-    }
-    this.getCSEFaculty = function(res) {
+            var facultyDetails = {
+                Designation: '',
+                Qualification: ''
+            }
+            $('.article-content table tr td table tr td:nth-child(2) p').filter(function()   {
+                 
+                 var info = $(this);
+                // var info = $('.article-content table tr td table tr td:nth-child(2) p');
+                 var infoText = info.text();
+                //  console.log(infoText);
 
+                 if(infoText.indexOf('Designation') > -1)   {
+                     facultyDetails.Designation = infoText.substring(24, infoText.length);
+                    }
+                 else if(infoText.indexOf('Qualification') > -1)   {
+                     facultyDetails.Qualification = infoText.substring(26, infoText.length);
+                 }
+                 else if(infoText.indexOf('Total') > -1)    {
+                    facultyDetails.exp = infoText.substring(39, infoText.length);
+                 	facultyArr.push(facultyDetails);
+                     facultyDetails = {
+                     Designation: '',
+                     Qualification: ''
+                    }
+                 }  
+
+             });
+             ITfaculty.details = facultyArr;
+        });
+         setTimeout((function() {res.send(ITfaculty)}), 2000);
+    }
+
+    this.getCSEFaculty = function(res) {
+        var facultyArr = [];
+        var arr = [];
         request({
             url: 'http://cse.mait.ac.in/index.php/people/faculty',
             method: "GET", 
@@ -50,23 +76,49 @@ function scrape()    {
             }
             //Scrapes all the faculty names
             $ = cheerio.load(body);
-            var arr = [];
             for(var i = 0; i < $('.article-content td a').length; i++)  {
                 var facultyName;
                 facultyName = $('.article-content td a').eq(i).text();
                 arr.push(facultyName);
-                // facultyObject.faculty_IT = facultyName;
-                // console.log(arr);
             }
             facultyObject = {};
             for(j = 0; j < arr.length; j++) {
                 facultyObject[(j+1)] = arr[j];
             }
             CSEfaculty.CSE = facultyObject;
-            console.log(CSEfaculty);
+            // console.log(CSEfaculty);
+            var facultyDetails = {
+                Designation: '',
+                Qualification: ''
+            }
+ 
+             $('.article-content table tr td table tr td:nth-child(2) p').filter(function()   {
+                
+                 var info = $(this);
+                 var infoText = info.text();
+                
+                 if(infoText.indexOf('Designation') > -1)   {
+                     facultyDetails.Designation = infoText.substring(24, infoText.length);
+                    }
+                 else if(infoText.indexOf('Qualification') > -1)   {
+                     facultyDetails.Qualification = infoText.substring(26, infoText.length);
+                 }
+                 else if(infoText.indexOf('Total') > -1)    {
+                    facultyDetails.exp = infoText.substring(39, infoText.length);
+                 	facultyArr.push(facultyDetails);
+                     facultyDetails = {
+                     Designation: '',
+                     Qualification: ''
+                    }
+                 }  
+
+             })
+             CSEfaculty.details = facultyArr;
+
         });
-         setTimeout((function() {res.send(CSEfaculty)}), 1500);
+         setTimeout((function() {res.send(CSEfaculty)}), 2000);
     }
+
 
     //Scraping all the B.Tech notices from ipu.ac.in
     this.getBtechNotices = function(res){
@@ -87,8 +139,7 @@ function scrape()    {
                 var txt = 'B. Tech';
                 var txt1 = 'B.Tech';
                 if(contentText.indexOf(txt) > -1 || contentText.indexOf(txt) > -1)   {
-                    // notice.push(contentText);
-                    // noticeLink.push(content.attr('href'))
+
                     noticeObject.name = contentText;
                     noticeObject.url = content.attr('href');
                     notice.push(noticeObject);
